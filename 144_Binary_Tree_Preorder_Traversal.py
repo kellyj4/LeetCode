d0ea59040fe253de17db1d5b171dbd81b6cf16e8
James Kelly <kellyj4@users.noreply.github.com>
2019-11-13 22:13:57 +1100

    Cleaned up whitespace

--- 144_Binary_Tree_Preorder_Traversal.py
+++ 144_Binary_Tree_Preorder_Traversal.py
@@ -6,21 +6,21 @@
 #         self.right = None
 
 class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:    
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
         preorder_result = []
-        
+
         if root is None:
             return preorder_result
-        
+
         preorder_result.append(root.val)
-        
+
         if root.left is not None:
             preorder_result.extend(self.preorderTraversal(root.left))
-        
+
         if root.right is not None:
             preorder_result.extend(self.preorderTraversal(root.right))
-        
+
         if root.left is None and root.right is None:
             return preorder_result
-        
+
         return preorder_result
